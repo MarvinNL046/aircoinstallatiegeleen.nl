4d7d5322841a07919fa29337f19f80f77325470b
Marvin046 <info@staycoolairco.nl>
2025-03-16 15:06:24 +0100

    Update domain references from aircoinstallatie-maastricht.nl to aircoinstallatiegeleen.nl

---
+++ next-sitemap.config.js
@@ -0,0 +1,30 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://aircoinstallatiegeleen.nl",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/_next/',
+          '/private/',
+        ],
+      },
+    ],
+    additionalSitemaps: [
+      "https://aircoinstallatiegeleen.nl/sitemap-locations.xml",
+      "https://aircoinstallatiegeleen.nl/sitemap-services.xml",
+    ],
+  },
+  exclude: ['/404', '/500', '/offline'],
+  generateIndexSitemap: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  autoLastmod: true,
+}
